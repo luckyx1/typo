d902ee8cfdbfe183413aa95a4327bced37d2ffe8
saasbook <saasbook@saasbook.(none)>
2013-11-02 18:21:14 -0700

    steps to do this hw

---
+++ features/step_definitions/merge_articles_steps.rb
@@ -0,0 +1,15 @@
+Given /^I am not the admin$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am the admin$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)" and "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)" with "(.*?)" as "(.*?)"$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
