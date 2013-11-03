4175098378f5f9a2a308b0dcbdacb52db0fe360f
saasbook <saasbook@saasbook.(none)>
2013-11-03 01:12:56 -0800

    oops forgot to push this

--- features/step_definitions/merge_articles_steps.rb
+++ features/step_definitions/merge_articles_steps.rb
@@ -1,9 +1,18 @@
 Given /^I am not the admin$/ do
-  pending # express the regexp above with the code you wish you had
+   # express the regexp above with the code you wish you had
+   if page.logged_in?
+    false
+  else
+    true
+  end
 end
 
 Given /^I am the admin$/ do
-  pending # express the regexp above with the code you wish you had
+  if page.responds_to? visit '/accounts/admin'
+    true
+  else
+    false
+  end
 end
 
 Then /^I should see "(.*?)" and "(.*?)"$/ do |arg1, arg2|
@@ -12,4 +21,13 @@ end
 
 Then /^I should see "(.*?)" with "(.*?)" as "(.*?)"$/ do |arg1, arg2, arg3|
   pending # express the regexp above with the code you wish you had
+end
+
+Given /the following (.*?) exist:$/ do |type, table|
+  table.hashes.each do |element|
+    if    type == "users"    then User.create(element)
+    elsif type == "articles" then Article.create(element)
+    elsif type == "comments" then Comment.create(element)
+    end
+  end
 end
